2ba9ad73e3b8dd0ff6100ae3b5a6747e402f3a11
Jeremiah <jayonojah@gmail.com>
2024-09-18 15:16:12 +0100

    Project Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>PixelPad</div>;
+  return (
+    <>
+      <Button>Click Me</Button>
+    </>
+  );
 }
